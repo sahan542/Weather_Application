ecd262c60687a711d4783b90f049e8140aceaa42
sahan542 <sahanrashmika542@gmail.com>
2023-09-27 13:33:51 +0530

    current-weather-card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import CurrentWeather from './components/current-weather/current-weather';
+import { WEATHER_API_URL,WEATHER_API_KEY} from './components/api';
 
 function App() {
+/*
+  const handleOnSearchChange = (searchData) => {
+    const [lat,lon] = searchData.value.split("");
+
+    const currentweatherFetch = fetch("${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}");
+    const forecastFetch = fetch("${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}");
+
+    promise.all([currentweatherFetch,forecastFetch])
+    .then(async (response) => {
+      const weatherResponse = await response[0].json();
+      const forecastResponse = await response[1].json();
+    })
+    
+  }
+  */
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <CurrentWeather />
     </div>
   );
 }
