0a0ae833ed1264280374e1911a76dfd67bb06347
sahan542 <sahanrashmika542@gmail.com>
2023-09-27 18:44:18 +0530

    forecast styles

--- src/components/forecast/forecast.js
+++ src/components/forecast/forecast.js
@@ -32,15 +32,15 @@ const Forecast = (data) =>{
                         <div className="daily-details-grid">
                             <div className="daily-details-grid-item">
                                 <label>Pressure</label>
-                                <label>{item.main.pressure}</label>
+                                <label>{item.main.pressure}hPa</label>
                             </div>
                             <div className="daily-details-grid-item">
                                 <label>Humidity</label>
-                                <label>{item.main.humidity}</label>
+                                <label>{item.main.humidity}%</label>
                             </div>
                             <div className="daily-details-grid-item">
                                 <label>Clouds</label>
-                                <label>{item.clouds.all}</label>
+                                <label>{item.clouds.all}%</label>
                             </div>
                             <div className="daily-details-grid-item">
                                 <label>Wind Speed</label>
